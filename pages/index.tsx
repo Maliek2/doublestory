bcbcd54596f54f9f669d228a80e70ce0c81e1461
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-09-27 00:01:32 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,20 +12,10 @@ const Home = () => {
 
         
       </Head>
-       <h1>Blog News</h1>
-  <amp-story>
-  <amp-story-auto-ads>
-    <script type="application/json">
-      {
-        "ad-attributes": {
-          // ad server configuration
-        }
-      }
-    </script>
-  </amp-story-auto-ads>
-  <amp-story-page>
+      <h1>Blog News</h1>
+  
  
-</amp-story>
+
       
 
         
