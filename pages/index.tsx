20a53b40a54cf889aed695ecfd26a2aef3363234
Maliek2 <104944194+Maliek2@users.noreply.github.com>
2022-09-29 23:19:02 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,7 @@ const Home = () => {
     <>
       <Head>
         <title>Web Design Theme</title>
-        <iframe>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6607653375286321"crossorigin="anonymous"></script>
-        </iframe>
+        
 
         
       </Head>
